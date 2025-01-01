16c694a0133adf776e52d7dbe621fc38475ec54c
Dennis Francis <dennisfrancis.in@gmail.com>
2025-01-02 00:16:33 +0530

    implement show_usage() and define Parameters class
    Signed-off-by: Dennis Francis <dennisfrancis.in@gmail.com>

--- src/cmdline.py
+++ src/cmdline.py
@@ -2,9 +2,20 @@
 
 """Utilities to parse command-line arguments and show usage"""
 
+from actions import *
+
+class Parameters:
+    action = ActionBase()
+    args: list[str] = []
+
 def get_params(args: list[str]):
     return None
 
+def _get_action_names():
+    return [action.name.lower() for action in ActionType if action != ActionType.UNKNOWN]
+
 def show_usage():
+    print("Usage: task-tracker <action> <action-arguments...>")
+    print("\nWhere action can be one of {}".format(_get_action_names()))
     return
 
